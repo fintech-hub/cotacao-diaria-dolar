0ab00e28b16572ddc75f6388573a5c8779ba0325
Matheus Muriel <matheus.muriel@outlook.com>
2020-10-02 17:04:04 -0300

    Implementado request por periodo

--- sample.py
+++ sample.py
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from datetime import date
+from dolar import Dolar, ModosDeConsulta
 
-from dolar import Dolar
+day = date(2020, 10, 2)
+periodo = {
+  "inicio": date(2020, 9, 28),
+  "final": date(2020, 10, 2)
+}
 
-TODAY = date(2020, 10, 2)
-cotacao = Dolar(TODAY)
+cotacao = Dolar(mode=ModosDeConsulta.PorDia, data=day)
+#cotacao = Dolar(mode=ModosDeConsulta.PorPeriodo, periodo=periodo)
 
 print(f'Compra PTAX: {cotacao.dolar_compra_ptax()}')
 print(f'Venda PTAX: {cotacao.dolar_venda_ptax()}')
--- test_dolar.py
+++ test_dolar.py
@@ -4,37 +4,46 @@
 import unittest
 from datetime import date
 
-from dolar import Dolar
+from dolar import Dolar, ModosDeConsulta
 
 
 class TestCase(unittest.TestCase):
 
     def setUp(self):
         TODAY = date(2020, 10, 1)
-        self.cotacao = Dolar(TODAY)
+        periodo = {
+            "inicio": date(2020, 9, 28),
+            "final": date(2020, 10, 2)
+        }
+        self.cotacaoDia = Dolar(mode=ModosDeConsulta.PorDia, data=TODAY)
+        self.cotacaoPeriodo = Dolar(mode=ModosDeConsulta.PorPeriodo, periodo=periodo)
 
     def test_dolar_compra_ptax(self):
-        self.assertIsNotNone(self.cotacao.dolar_compra_ptax())
+        self.assertIsNotNone(self.cotacaoDia.dolar_compra_ptax())
+        self.assertIsNotNone(self.cotacaoPeriodo.dolar_compra_ptax())
 
     def test_dolar_compra_maior_zero(self):
-        self.assertTrue(self.cotacao.dolar_compra_ptax() > 0)
+        self.assertTrue(self.cotacaoDia.dolar_compra_ptax() > 0)
+        self.assertTrue(self.cotacaoPeriodo.dolar_compra_ptax() > 0)
 
     def test_dolar_venda_ptax(self):
-        self.assertIsNotNone(self.cotacao.dolar_venda_ptax())
+        self.assertIsNotNone(self.cotacaoDia.dolar_venda_ptax())
+        self.assertIsNotNone(self.cotacaoPeriodo.dolar_venda_ptax())
 
     def test_dolar_venda_ptax_maior_zero(self):
-        self.assertTrue(self.cotacao.dolar_venda_ptax() > 0)
+        self.assertTrue(self.cotacaoDia.dolar_venda_ptax() > 0)
+        self.assertTrue(self.cotacaoPeriodo.dolar_venda_ptax() > 0)
 
     def test_ultimacotacao_exist(self):
         self.assertIsNotNone(self.cotacao.dolar_ultimacotacao())
 
     def test_is_not_weekday(self):
-        self.assertFalse(self.cotacao.is_weekday())
+        self.assertFalse(self.cotacaoDia.is_weekday())
 
     def test_is_weekday(self):
         TODAY = date(2020, 10, 3)
-        self.cotacao = Dolar(TODAY)
-        self.assertTrue(self.cotacao.is_weekday())
+        self.cotacaoDia = Dolar(mode=ModosDeConsulta.PorDia, data=TODAY)
+        self.assertTrue(self.cotacaoDia.is_weekday())
 
 
 if __name__ == '__main__':
